22969bd763386d260adad1769e09272e0e015a46
Gummientchen <info@recode.ch>
2020-10-17 02:12:55 +0200

    YouTube End Card Black
    add 20 seconds black at the end of the video for YouTube end card

--- convert.bat
+++ convert.bat
@@ -1,5 +1,5 @@
 echo off
-"%~dp0ffmpeg.exe" -i "%~1" -c:v libx264 -preset veryfast -profile:v high -crf 18 -coder 1 -pix_fmt yuv420p -movflags +faststart -g 30 -bf 2 -vf setpts=8*PTS,scale=3840:2160:flags=lanczos "%~1"_recoded.mp4
+"%~dp0ffmpeg.exe" -i "%~1" -c:v libx264 -preset veryfast -profile:v high -crf 18 -coder 1 -pix_fmt yuv420p -movflags +faststart -g 30 -bf 2 -vf setpts=8*PTS,scale=3840:2160:flags=lanczos,tpad=stop_duration=20 "%~1"_recoded.mp4
 rem "%~dp0ffmpeg.exe" -i "%~1" -c:v libx264 -preset fast -profile:v high -crf 18 -coder 1 -pix_fmt yuv420p -movflags +faststart -g 30 -bf 2 -c:a aac -b:a 384k -profile:a aac_low -vf setsar_sar=0 -vf setdar=dar=0 -vf scale=3840:2160:flags=lanczos "%~1"_recoded.mp4
 
 
