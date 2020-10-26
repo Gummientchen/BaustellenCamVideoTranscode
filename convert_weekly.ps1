594bca2524f48825792822102a513381d9647823
Gummientchen <info@recode.ch>
2020-10-26 19:41:15 +0100

    updated scripts to new yt ending

--- convert_weekly.ps1
+++ convert_weekly.ps1
@@ -6,6 +6,9 @@ $i = 0
 Set-Location weekly
 Get-ChildItem *.jpg | %{Rename-Item $_ -NewName ('{0:D5}.jpg' -f $i++)}
 
-..\ffmpeg.exe -r 60 -f image2 -s 1920x1080 -i %05d.jpg -vcodec libx264 -crf 20 -vf scale=3840:2160:flags=lanczos,tpad=stop_duration=20 -pix_fmt yuv420p ..\output\week.mp4
+..\ffmpeg.exe -r 25 -f image2 -s 1920x1080 -i %05d.jpg -vcodec libx264 -crf 20 -vf scale=3840:2160:flags=lanczos -pix_fmt yuv420p ..\output\weektmp.mp4
+..\ffmpeg.exe -i ..\output\weektmp.mp4 -i ..\yt_ending_screen.mp4 -c:v libx264 -preset veryfast -profile:v high -crf 20 -vsync 2 -pix_fmt yuv420p -movflags +faststart -g 30 -bf 2 -filter_complex "[0:v] [1:v] concat=n=2:v=1 [v]" -map "[v]" ..\output\week.mp4
+
+Remove-Item ..\output\weektmp.mp4
 
 pause
